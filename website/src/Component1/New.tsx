7b8c6c74aa4b4b1af09e9ede3f50961a0aa598d1
vinayaknaik072 <vinayaknaik072@gmail.com>
2026-02-18 23:02:16 +0530

    first commit

--- website/src/Component1/New.tsx
+++ website/src/Component1/New.tsx
@@ -1,10 +1,36 @@
 import "./New.css"
+import img from '../assets/img27.jpg'
 function New(){
 return(
     <>
      <div className="main">
-        <div className="top"></div>
-        <div className="bottom"></div>
+        <div className="top">
+            <div className="l-1"></div>
+        </div>
+        <div className="bottom">
+            <div className="r-1">
+                <div className="top1">
+                    <div className="left">
+                        <input type="search"className="search" />
+                    </div>
+                    <div className="right"></div>
+                </div>
+                <div className="bottom1">
+                    <div className="top2">
+                        <img src={img} alt=""srcSet="" className="img"/></div>
+                    <div className="bottom2">
+                        <div className="b1"><h3>Kabza</h3></div>
+                        <div className="b2"><h4>Language:English</h4></div>
+                        <div className="b3">
+                            <div className="left2"><h3>Genre</h3></div>
+                            <h3>:</h3>
+                            <div className="right2"><h4>2022-04-22</h4></div>
+                        </div>
+                        <div className="b4"><h4>A Viking price seeks revenge</h4></div>
+                    </div>
+                </div>
+            </div>
+        </div>
      </div>
     </>
 
