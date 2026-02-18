cffba496db6f42d69c1473f82b629ea6bbada65e
vinayaknaik072 <vinayaknaik072@gmail.com>
2026-02-18 20:30:33 +0530

    first commit

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <div className="main">
+    <div className="app">
 
     </div>
      <New/>
---
+++ website/src/Component1/New.tsx
@@ -0,0 +1,13 @@
+import "./New.css"
+function New(){
+return(
+    <>
+     <div className="main">
+        <div className="top"></div>
+        <div className="bottom"></div>
+     </div>
+    </>
+
+)
+}
+export default New
